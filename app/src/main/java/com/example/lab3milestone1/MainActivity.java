e25988e6e4b66c0f2056a30ecc4f2a79223de899
John Bednarczyk <johnbednarczykjr@gmail.com>
2020-02-14 09:19:42 -0600

    Milestone 1

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.lab3milestone1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button dialogButton;
+
+    /**
+     * This method is the general onCreate for anything non-specific we put on the screen.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dialogButton = findViewById(R.id.dialogButton);
+
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+
+    /**
+     * This onCreate is specific to the menu.
+     *
+     * @param menu the menu we are creating
+     * @return true if the menu is created
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this,"Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this,"Item 3 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subItem1:
+                Toast.makeText(this,"Sub Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subItem2:
+                Toast.makeText(this,"Sub Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    /**
+     * This method is whats called when we click the button.
+     */
+    private void openDialog() {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
     }
 }
